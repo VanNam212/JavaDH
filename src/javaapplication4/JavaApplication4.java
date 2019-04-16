9e31db606365b95082aa1907c2bc36c0848c2384
VanNam212 <48545931+VanNam212@users.noreply.github.com>
2019-04-16 19:49:23 +0700

    edit src

--- src/javaapplication4/JavaApplication4.java
+++ src/javaapplication4/JavaApplication4.java
@@ -17,6 +17,7 @@ public class JavaApplication4 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Abc");
+        System.out.println("xyz");
     }
     
 }
